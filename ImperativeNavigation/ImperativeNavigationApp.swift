8e7daa1f5551331938e3c372b84c5bd89dac5907
Ahmed M. Hassan <ahmdmhasn@gmail.com>
2024-10-12 15:53:39 +0300

    Initial Commit

---
+++ ImperativeNavigation/ImperativeNavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImperativeNavigationApp.swift
+//  ImperativeNavigation
+//
+//  Created by Ahmed M. Hassan on 12/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct ImperativeNavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
