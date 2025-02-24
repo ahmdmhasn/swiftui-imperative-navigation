a5b8b5f2216c41a046b31becd0eea072d4dc2929
Ahmed M. Hassan <ahmdmhasn@gmail.com>
2025-02-24 22:13:31 +0200

    Drop macOS support

--- Package.swift
+++ Package.swift
@@ -6,7 +6,6 @@ import PackageDescription
 let package = Package(
     name: "ImperativeNavigation",
     platforms: [
-        .macOS(.v13),
         .iOS(.v16),
         .watchOS(.v9),
         .tvOS(.v16),
