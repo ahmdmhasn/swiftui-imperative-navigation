3e15074393d2533da986afb740cd88ac84dde2c5
Ahmed M. Hassan <ahmdmhasn@gmail.com>
2025-02-24 22:10:05 +0200

    Support VisionOS

--- Package.swift
+++ Package.swift
@@ -9,7 +9,8 @@ let package = Package(
         .macOS(.v13),
         .iOS(.v16),
         .watchOS(.v9),
-        .tvOS(.v16)
+        .tvOS(.v16),
+        .visionOS(.v1)
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
